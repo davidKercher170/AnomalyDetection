057fdbfcc7e7cd54b3f8f9f417cb65a938ec0f65
David Kercher <55001712+davidKercher170@users.noreply.github.com>
2025-04-03 14:18:42 -0600

    Update Autoencoder.py

--- Autoencoder.py
+++ Autoencoder.py
@@ -1,18 +1,5 @@
-class AutoEncoder(tf.keras.Model):
-    def __init__(self, data_dim=61, latent_dim=10):
-        super(AutoEncoder, self).__init__()
-        self.data_dim = data_dim
-        self.latent_dim = latent_dim
-        self.encoder = self.build_encoder()
-        self.decoder = self.build_decoder()
-        self.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
+class AutoEncoder(BaseAutoEncoder):
 
-    def call(self, inputs):
-        encoded = self.encoder(inputs)
-        decoded = self.decoder(encoded)
-        return decoded
-
-    # Encoder Data Dimension -> Latent Dimension
     def build_encoder(self):
         # Encoder network: 128 -> 64 -> 32 -> Latent Dimension (mean, log_var)
         inputs = layers.Input(shape=(self.data_dim,))
@@ -31,29 +18,23 @@ class AutoEncoder(tf.keras.Model):
         encoder = models.Model(inputs, latent, name="encoder")
         return encoder
 
-    # Decoder: Latent Dimension -> Data Dimension
-    def build_decoder(self):
-        latent_inputs = layers.Input(shape=(self.latent_dim,))
-        x = layers.Dense(32, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(latent_inputs)
-        x = layers.BatchNormalization()(x)
+    @tf.function
+    def train_step(self, data):
+        # If the data comes as a tuple, we only need the inputs.
+        if isinstance(data, tuple):
+            data = data[0]
 
-        x = layers.Dense(64, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
-        x = layers.BatchNormalization()(x)
+        with tf.GradientTape() as tape:
+            # Forward pass: get reconstruction, mean, and log variance
+            reconstructed = self(data)
 
-        x = layers.Dense(128, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
-        x = layers.BatchNormalization()(x)
+            # Compute reconstruction loss using Mean Squared Error
+            reconstruction_loss = tf.reduce_mean(tf.keras.losses.mse(data, reconstructed))
 
-        decoded = layers.Dense(self.data_dim, activation='tanh')(x)
-        decoder = models.Model(latent_inputs, decoded, name="decoder")
-        return decoder
+            # Multiply by the number of data dimension to scale up the loss.
+            reconstruction_loss *= self.data_dim
 
-    # Inputs: Preprocessed Data, Data: Raw Data to label, Alpha: STD Scalar
-    def find_anomalies(self, inputs, data, alpha=2):
-        reconstructed_data = self(inputs) # Reconstruct the data using the model
-        reconstruction_error = np.mean(np.square(reconstructed_data-inputs), axis=1) # Calculate Reconstruction Error
-        threshold = np.mean(reconstruction_error) + alpha*np.std(reconstruction_error) # Determine Anomlay Threshold from Distribution of Errors
-        anomalies = reconstruction_error > threshold
-        data['FRAUD_PRED'] = anomalies
-        data_sorted = data[data['FRAUD_PRED'] == True].sort_values(by='FRAUD_PRED')
-        data_sorted.to_csv('ae_results.csv')
-        print("Anomaly Count: ", len(data_sorted))
+        # Compute gradients and update weights
+        grads = tape.gradient(reconstruction_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        return {"loss": reconstruction_loss}
