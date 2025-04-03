07e3a8ddd36a8b11fef68e9eae5215b4c6031d97
David Kercher <55001712+davidKercher170@users.noreply.github.com>
2025-04-03 14:19:46 -0600

    Create ContrastiveVAE.py

---
+++ ContrastiveVAE.py
@@ -0,0 +1,75 @@
+class ContrastiveVAE(ContrastiveAutoEncoder):
+    def __init__(self, data_dim=61, latent_dim=10, reconstruction_weight=1.0, contrastive_weight=1.0, beta=1.0):
+        super().__init__(data_dim, latent_dim, reconstruction_weight, contrastive_weight, name="cvae")
+        self.beta = beta
+
+    def call(self, inputs, training=True):
+        z_mean, z_log_var = self.encoder(inputs)
+        z = self.sampling([z_mean, z_log_var])
+        reconstructed = self.decoder(z)
+        return reconstructed
+
+    def build_encoder(self):
+        # Encoder network: Data Dimension -> 128 -> 64 -> 32 -> Latent Dimension (mean, log_var)
+        inputs = layers.Input(shape=(self.data_dim,))
+
+        x = layers.Dense(128, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(inputs)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Dense(64, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Dense(32, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
+        x = layers.BatchNormalization()(x)
+
+        # Latent Space Parameters - Create a Distribution using Predicted Mean and Standard Deviation
+        mean = layers.Dense(self.latent_dim)(x)
+        stddev = layers.Dense(self.latent_dim)(x)
+
+        encoder = models.Model(inputs, [mean, stddev], name="encoder")
+        return encoder
+
+    def sampling(self, args):
+        z_mean, z_log_var = args
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        z = z_mean + tf.exp(0.5 * z_log_var) * epsilon # Mean + Standard Deviation * Epsilon
+        return z
+
+    def get_latent_vector(self, inputs):
+        z_mean, z_log_var = self.encoder(inputs) # Get the Vectors from the Latent Dimension
+        return self.sampling([z_mean, z_log_var])
+
+    @tf.function
+    def train_step(self, x1, x2):
+        with tf.GradientTape() as tape:
+            z1 = self.encoder(x1) # Latent Representation 1
+            z1_mean, z1_log_var = self.encoder(x1)
+            z1 = self.sampling([z1_mean, z1_log_var])
+
+            z2 = self.encoder(x2) # Latent Representation 1
+            z2_mean, z2_log_var = self.encoder(x2)
+            z2 = self.sampling([z2_mean, z2_log_var])
+
+            # Reconstruction Loss
+            y1 = self.decoder(z1) # Reconstruction 1
+            reconstruction_loss = tf.reduce_mean(tf.keras.losses.mse(x1, y1)) * self.data_dim # Compute reconstruction loss using Mean Squared Error
+
+            # Compute KL divergence loss
+            kl_loss = -0.5 * tf.reduce_mean(
+                1 + z1_log_var - tf.square(z1_mean) - tf.exp(z1_log_var)
+            ) * self.beta
+
+            # Contrastive Loss
+            contrastive_loss = self.contrastive_loss(z1, z2) # Contrastive Loss
+            total_loss = self.reconstruction_weight*(reconstruction_loss + kl_loss) + self.contrastive_weight*contrastive_loss # Combine Losses
+
+        grads = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        # Return a dictionary mapping metric names to current loss values.
+        return {"loss": total_loss,
+                "reconstruction_loss": self.reconstruction_weight*reconstruction_loss,
+                "kl_loss": self.reconstruction_weight*kl_loss,
+                "contrastive_loss": self.contrastive_weight*contrastive_loss}
