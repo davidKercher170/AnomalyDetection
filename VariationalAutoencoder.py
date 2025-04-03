d35f2ab2a266dffc439ecb729349e85d60765e9c
David Kercher <55001712+davidKercher170@users.noreply.github.com>
2025-04-03 14:18:18 -0600

    Update VariationalAutoencoder.py

--- VariationalAutoencoder.py
+++ VariationalAutoencoder.py
@@ -1,20 +1,14 @@
-class VariationalAutoEncoder(tf.keras.Model):
-    def __init__(self, data_dim=61, latent_dim=10, beta=1.0):
-        super(VariationalAutoEncoder, self).__init__()
-        self.data_dim = data_dim
-        self.latent_dim = latent_dim
-        self.beta = beta # Disentaglement Parameter for D-VAE
-        self.encoder = self.build_encoder()
-        self.decoder = self.build_decoder()
-        self.compile(optimizer='adam')
+class VariationalAutoEncoder(BaseAutoEncoder):
+    def __init__(self, data_dim=61, latent_dim=10, beta=2.0):
+        super().__init__(data_dim, latent_dim, name="vae")
+        self.beta = beta
 
-    def call(self, inputs):
+    def call(self, inputs, training=True):
         z_mean, z_log_var = self.encoder(inputs)
         z = self.sampling([z_mean, z_log_var])
         reconstructed = self.decoder(z)
-        return reconstructed, z_mean, z_log_var
+        return reconstructed
 
-    # Encoder: Data Dimension -> Latent Dimension
     def build_encoder(self):
         # Encoder network: Data Dimension -> 128 -> 64 -> 32 -> Latent Dimension (mean, log_var)
         inputs = layers.Input(shape=(self.data_dim,))
@@ -35,25 +29,6 @@ class VariationalAutoEncoder(tf.keras.Model):
         encoder = models.Model(inputs, [mean, stddev], name="encoder")
         return encoder
 
-    # Decoder: Latent Dimension -> Data Dimension
-    def build_decoder(self):
-        # Decoder Network: Latent Dimension -> 32 -> 64 -> 128 -> Data Dimension (Reconstruct Vector)
-        latent_inputs = layers.Input(shape=(self.latent_dim,))
-        x = layers.Dense(32, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(latent_inputs)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Dense(64, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Dense(128, activation='swish', kernel_regularizer=tf.keras.regularizers.l2(0.0001))(x)
-        x = layers.BatchNormalization()(x)
-
-        # Reconstruct the Data of given Dimension
-        decoded = layers.Dense(self.data_dim, activation='sigmoid')(x)
-        decoder = models.Model(latent_inputs, decoded, name="decoder")
-        return decoder
-
-    # Sample the Latent Space using Mean, STD produced by the Encoder
     def sampling(self, args):
         z_mean, z_log_var = args
         batch = tf.shape(z_mean)[0]
@@ -62,6 +37,7 @@ class VariationalAutoEncoder(tf.keras.Model):
         z = z_mean + tf.exp(0.5 * z_log_var) * epsilon # Mean + Standard Deviation * Epsilon
         return z
 
+    @tf.function
     def train_step(self, data):
         # If the data comes as a tuple, we only need the inputs.
         if isinstance(data, tuple):
@@ -69,8 +45,9 @@ class VariationalAutoEncoder(tf.keras.Model):
 
         with tf.GradientTape() as tape:
             # Forward pass: get reconstruction, mean, and log variance
-            reconstructed, z_mean, z_log_var = self(data, training=True)
-
+            z_mean, z_log_var = self.encoder(data)
+            z = self.sampling([z_mean, z_log_var])
+            reconstructed = self.decoder(z)
             # Compute reconstruction loss using Mean Squared Error
             reconstruction_loss = tf.reduce_mean(tf.keras.losses.mse(data, reconstructed))
 
@@ -93,15 +70,3 @@ class VariationalAutoEncoder(tf.keras.Model):
         return {"loss": total_loss,
                 "reconstruction_loss": reconstruction_loss,
                 "kl_loss": kl_loss}
-
-
-    # Inputs: Preprocessed Data, Data: Raw Data to label, Alpha: STD Scalar
-    def find_anomalies(self, inputs, data, alpha=2):
-        reconstructed_data = self(inputs)[0] # Reconstruct the data using the model
-        reconstruction_error = np.mean(np.square(reconstructed_data-inputs), axis=1) # Calculate Reconstruction Error
-        threshold = np.mean(reconstruction_error) + alpha*np.std(reconstruction_error) # Determine Anomlay Threshold from Distribution of Errors
-        anomalies = reconstruction_error > threshold
-        data['FRAUD_PRED'] = anomalies
-        data_sorted = data[data['FRAUD_PRED'] == True].sort_values(by='FRAUD_PRED')
-        data_sorted.to_csv('vae_results.csv')
-        print("Anomaly Count: ", len(data_sorted))
